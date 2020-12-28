c71a74f292f1d655d8325c02faa93f312172ca12
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-28 07:28:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,3 +82,61 @@ def current_player(board)
     "O"
   end
 end
+
+def won?(board)
+  for x in WIN_COMBINATIONS
+    win_index_1 = x[0]
+    win_index_2 = x[1]
+    win_index_3 = x[2]
+    p1 = board[win_index_1]
+    p2 = board[win_index_2]
+    p3 = board[win_index_3]
+
+    if p1 == "X" && p2 == "X" && p3 == "X"
+      return x
+    elsif p1 == "O" && p2 == "O" && p3 == "O"
+      return x
+    end
+  end
+  false
+end
+
+def full?(board)
+  board.all?{|cell| cell != ' '}
+end
+
+def draw?(board)
+  if won?(board)
+    false
+  elsif full?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  for x in WIN_COMBINATIONS
+    win_index_1 = x[0]
+    win_index_2 = x[1]
+    win_index_3 = x[2]
+    p1 = board[win_index_1]
+    p2 = board[win_index_2]
+    p3 = board[win_index_3]
+
+    if p1 == "X" && p2 == "X" && p3 == "X"
+      return 'X'
+    elsif p1 == "O" && p2 == "O" && p3 == "O"
+      return 'O'
+    end
+  end
+  nil
+end
