bb41b83bf6ede9620ecbd58e8682c1dc971df403
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-28 07:20:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,3 +47,18 @@ def valid_move?(board, index)
     return false
   end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index) == true
+      move(board, index, "X")
+      display_board(board)
+    else
+      input = gets.strip
+      index = input_to_index(input)
+      valid_move?(board, index)
+      puts "Please enter 1-9:"
+    end
+end
