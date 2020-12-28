4ce50c56663c35b4a26b33e3f856f3442dc60c50
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-28 07:18:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,3 +24,16 @@ end
 def move(array, index, player = "X")
   array[index] = player
 end
+
+# code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] ==""
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return false
+  end
+end
